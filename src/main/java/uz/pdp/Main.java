4f02ea2ca9eb691e33587db62be2b88a55d5623c
Javoxir <shukurullayevjavoxir777@gmail.com>
2023-12-02 12:10:32 +0500

    TEST

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -1,7 +1,11 @@
 package uz.pdp;
 
+import uz.pdp.models.User;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Javohir");
+        User user = new User(2);
+        System.out.println(user);
     }
 }
---
+++ src/main/java/uz/pdp/models/User.java
@@ -0,0 +1,9 @@
+package uz.pdp.models;
+
+public class User {
+    private int age;
+
+    public User(int age) {
+        this.age = age;
+    }
+}
