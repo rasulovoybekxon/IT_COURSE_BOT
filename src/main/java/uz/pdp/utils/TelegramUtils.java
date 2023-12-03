2842c98d1851922085256849a294672521a9fd58
Oybek Rasulov <javafx@gmail.com>
2023-12-03 17:10:56 +0500

    create InlineMarkups and bot token and username

---
+++ src/main/java/uz/pdp/utils/TelegramUtils.java
@@ -0,0 +1,6 @@
+package uz.pdp.utils;
+
+public interface TelegramUtils {
+    String BOT_TOKEN = "6811590522:AAGjh1a0Lz9r3CAVSYGx6hXd7nyhWi0F_Eg";
+    String BOT_USER_NAME = "PDP_Course_fake_bot";
+}
