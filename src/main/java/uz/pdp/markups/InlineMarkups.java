dbf53f2f11af3805c9f6aff459fd10c579a6d9f4
Oybek Rasulov <javafx@gmail.com>
2023-12-05 10:57:55 +0500

    create InlineMarkups

--- src/main/java/uz/pdp/markups/InlineMarkups.java
+++ src/main/java/uz/pdp/markups/InlineMarkups.java
@@ -30,7 +30,6 @@ public class InlineMarkups {
 
             button = new InlineKeyboardButton(resultSet.getString("course_name"));
             button.setCallbackData(resultSet.getString("course_name"));
-            button.setCallbackData("Back");
             row.add(button);
             rows.add(row);
             row = new ArrayList<>();
@@ -43,9 +42,183 @@ public class InlineMarkups {
         List<List<InlineKeyboardButton>> rows = new ArrayList<>();
         List<InlineKeyboardButton> row = new ArrayList<>();
 
-        InlineKeyboardButton button = new InlineKeyboardButton("1-lesson. Programming computer hardware and software");
+        InlineKeyboardButton button = new InlineKeyboardButton("1-dars. Dasturlash kompyuterning texnik va dasturiy ta'minoti");
 
-        button.setCallbackData("1-lesson. Programming computer hardware and software");
+        button.setCallbackData("1-dars.Dasturlash kompyuterning texnik va dasturiy ta'minoti");
+        row.add(button);
+        rows.add(row);
 
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("2-dars.Ma'lumot va axborot.Axborotning o`lchov birliklari-Sanoq");
+        button.setCallbackData("2-dars.Ma'lumot va axborot.Axborotning o`lchov birliklari-Sanoq");
+
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("3-dars.C++ da ma'lumot turlari,o`zfaruvchilar va literallar");
+        button.setCallbackData("3-dars.C++ da ma'lumot turlari,o`zfaruvchilar va literallar");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("4-dars.C++ dasturlash tili  asosiy operatirlari");
+        button.setCallbackData("4-dars.C++ dasturlash tili  asosiy operatirlari");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("5-dars.Algoritm tushunchasi va turlari.Chiziqliy algoritmlar");
+        button.setCallbackData("5-dars.Algoritm tushunchasi va turlari.Chiziqliy algoritmlar");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+
+        button = new InlineKeyboardButton("6-dars.Mantiqiy amallar");
+        button.setCallbackData("6-dars.Mantiqiy amallar");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("7-dars.Massivlar bilan ishlash");
+        button.setCallbackData("7-dars.Massivlar bilan ishlash");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("8-dars.Shart operatori");
+        button.setCallbackData("8-dars.Shart operatori");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("9-dars.Tanlash operatori");
+        button.setCallbackData("9-dars.Tanlash operatori");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("10-dars.Takrorlanuvchi operatorlar");
+        button.setCallbackData("10-dars.Takrorlanuvchi operatorlar");
+        row.add(button);
+        rows.add(row);
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("⬅\uFE0F");
+        button.setCallbackData("⬅\uFE0F");
+        row.add(button);
+
+        button = new InlineKeyboardButton("➡\uFE0F");
+        button.setCallbackData("➡\uFE0F");
+        row.add(button);
+        rows.add(row);
+
+        return InlineKeyboardMarkup.builder().keyboard(rows).build();
+    }
+
+    public InlineKeyboardMarkup models2() {
+
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+
+        InlineKeyboardButton button = new InlineKeyboardButton("11-dars.Takrorlanuvchi algoritmlar");
+
+        button.setCallbackData("11-dars.Takrorlanuvchi algoritmlar");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("12-dars.C++ da takrorlash operatorlari (1)");
+        button.setCallbackData("12-dars.C++ da takrorlash operatorlari (1)");
+
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("12-dars.C++ da takrorlash operatorlari (2)");
+        button.setCallbackData("12-dars.C++ da takrorlash operatorlari (2)");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("13-dars.Funksiya va prosedura tushunchasi.Kutibxonalari");
+        button.setCallbackData("13-dars.Funksiya va prosedura tushunchasi.Kutibxonalari");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("14-dars.Funksiya bilan ishlash.User-defined functions");
+        button.setCallbackData("14-dars.Funksiya bilan ishlash.User-defined functions");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+
+        button = new InlineKeyboardButton("15-dars.Function overloading");
+        button.setCallbackData("15-dars.Function overloading");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("16-dars.Recursive function");
+        button.setCallbackData("16-dars.Recursive function");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("17-dars.Massvlar bilan ishlash (1)");
+        button.setCallbackData("17-dars.Massvlar bilan ishlash (1)");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("18-dars.Massvlar bilan ishlash (2)");
+        button.setCallbackData("18-dars.Massvlar bilan ishlash (2)");
+        row.add(button);
+        rows.add(row);
+
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("19-dars.Satrlar bilan ishlash (1)");
+        button.setCallbackData("19-dars.Satrlar bilan ishlash (1)");
+        row.add(button);
+        rows.add(row);
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("20-dars.Satrlar bilan ishlash (2)");
+        button.setCallbackData("20-dars.Satrlar bilan ishlash (2)");
+        row.add(button);
+        rows.add(row);
+        row = new ArrayList<>();
+
+        button = new InlineKeyboardButton("⬅\uFE0F");
+        button.setCallbackData("⬅\uFE0F");
+        row.add(button);
+
+        button = new InlineKeyboardButton("➡\uFE0F");
+        button.setCallbackData("➡\uFE0F");
+        row.add(button);
+        rows.add(row);
+
+
+        return InlineKeyboardMarkup.builder().keyboard(rows).build();
     }
 }
--- src/main/java/uz/pdp/service/BotService.java
+++ src/main/java/uz/pdp/service/BotService.java
@@ -1,6 +1,8 @@
 package uz.pdp.service;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import uz.pdp.markups.InlineMarkups;
 import uz.pdp.markups.MarkUps;
 
@@ -8,14 +10,56 @@ public class BotService {
     private InlineMarkups inlineMarkups = new InlineMarkups();
 
     public SendMessage requestContact(String chatId) {
-        SendMessage sendMessage = new SendMessage(chatId, "Please share your phone number");
+        SendMessage sendMessage = new SendMessage(chatId, "\t Welcome to our IT course \t Please share contact");
         sendMessage.setReplyMarkup(MarkUps.contactButton());
         return sendMessage;
     }
 
-    public SendMessage categoryCourse(String chatId, String category) {
+    public SendMessage categoryAvailableCourse(String chatId, String category) {
         SendMessage sendMessage = new SendMessage(chatId, "Select");
         sendMessage.setReplyMarkup(inlineMarkups.availableCourses(category));
         return sendMessage;
+    }
+
+    public SendMessage showCategory(String chatId) {
+        SendMessage sendMessage = new SendMessage(chatId, "Select one of them");
+        sendMessage.setReplyMarkup(MarkUps.showCategories());
+        return sendMessage;
+    }
+
+    public SendMessage showMainManu(String chatId) {
+        SendMessage sendMessage = new SendMessage(chatId, "MAIN MANU");
+        sendMessage.setReplyMarkup(MarkUps.mainManu());
+        return sendMessage;
+    }
+
+    public SendMessage startCourses(String chatId) {
+        SendMessage sendMessage = new SendMessage(chatId, "Select one of them");
+        sendMessage.setReplyMarkup(MarkUps.startCourse());
+        return sendMessage;
+    }
+
+    public SendMessage showLessons(String chatId) {
+        SendMessage sendMessage = new SendMessage(chatId, "Your lessons");
+        sendMessage.setReplyMarkup(inlineMarkups.models());
+        return sendMessage;
+    }
+
+    public SendMessage showLessons2(String chatId) {
+        SendMessage sendMessage = new SendMessage(chatId, "Your lessons");
+        sendMessage.setReplyMarkup(inlineMarkups.models());
+        return sendMessage;
+    }
+
+    public EditMessageText editShowLesson2(String chatId, Integer messageId, String inlineMessageId) {
+//        EditMessageReplyMarkup markup = new EditMessageReplyMarkup(chatId, messageId, inlineMessageId, new InlineKeyboardMarkup());
+        EditMessageText messageText = new EditMessageText();
+        messageText.setText("Part 2");
+        messageText.setChatId(chatId);
+        messageText.setInlineMessageId(inlineMessageId);
+        messageText.setMessageId(messageId);
+        messageText.setReplyMarkup(new InlineKeyboardMarkup());
+        return messageText;
+
     }
 }
